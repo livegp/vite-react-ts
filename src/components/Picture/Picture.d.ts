e6c0eb50890a4e448dfeb9449909d7001aaaf0cb
Pishta Oleksandr <live.gp@gmail.com>
2024-10-26 17:24:22 +0300

    fix

--- src/components/Picture/Picture.d.ts
+++
@@ -1,3 +0,0 @@
-// declare module '*.png?as=picture&w=500' {
-//   export const picture: PictureType;
-// }
--- src/ui/SvgIcon.jsx
+++
@@ -1,8 +0,0 @@
-export function SvgIcon({ name, prefix = 'icon', color = '#333', ...props }) {
-  const symbolId = `#${prefix}-${name}`;
-  return (
-    <svg {...props} aria-hidden='true'>
-      <use href={symbolId} fill={color} />
-    </svg>
-  );
-}

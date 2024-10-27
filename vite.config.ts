6f166b1cc5fb7f56be34606802eeee2a8abcad13
Pishta Oleksandr <live.gp@gmail.com>
2024-10-27 22:43:18 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
       'process.env': env,
     },
     base: env.VITE_BASE_URL || '/',
-    cacheDir: './node_modules/.cache/.vite',
+    cacheDir: 'node_modules/.cache/.vite',
     server: {
       port: mode === 'development' ? 3000 : 8080,
       strictPort: false,
@@ -37,10 +37,6 @@ export default defineConfig(({ mode }) => {
     css: {
       transformer: 'lightningcss',
       devSourcemaps: true,
-      // lightningcss: {
-      //   minify: true,
-      //   sourceMap: true,
-      // },
     },
     appType: 'spa',
     plugins: [

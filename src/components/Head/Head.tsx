cbbdb0f51f3d6bc2dc7f576f21a7de8ae6175555
Pishta Oleksandr <live.gp@gmail.com>
2024-11-10 16:19:17 +0200

    fix

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 export const Head: FC = () => (
-  <Helmet>
+  <Helmet prioritizeSeoTags={true}>
     <meta charSet='UTF-8' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <meta name='author' content='Oleksandr Pishta' />

1b7542c1c8af3fe59b8add66b50bac1fab6da260
livegp <113643375+livegp@users.noreply.github.com>
2024-12-09 20:54:40 +0000

    Deploying to gh-pages from @ livegp/vite-react-ts@4d9a5fcd78cfaa64a127b199bc991412e940ebcf 🚀

--- assets/ErrorTest-BOaZsfvv.js
+++
@@ -1,2 +0,0 @@
-import{r,j as o}from"./main-Bj-cb6f1.js";const e=()=>(r.useEffect(()=>{throw new Error("Це тестова помилка для перевірки ErrorFallback")},[]),o("div",{children:"Цей текст ви не побачите, бо буде викинута помилка"}));export{e as ErrorTest};
-//# sourceMappingURL=ErrorTest-BOaZsfvv.js.map
---
+++ assets/ErrorTest-D0LPHmAV.js
@@ -0,0 +1,2 @@
+import{r,j as o}from"./main-CUvI2d73.js";const e=()=>(r.useEffect(()=>{throw new Error("Це тестова помилка для перевірки ErrorFallback")},[]),o("div",{children:"Цей текст ви не побачите, бо буде викинута помилка"}));export{e as ErrorTest};
+//# sourceMappingURL=ErrorTest-D0LPHmAV.js.map
